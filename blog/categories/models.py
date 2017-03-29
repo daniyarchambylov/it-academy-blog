f7a2ce90f1a1b4e9f1e243ec2dc8565ef5c1987d
Daniyar Chambylov <dan.chambylov@gmail.com>
2017-03-30 01:00:29 +0600

    add category model

--- blog/categories/models.py
+++ blog/categories/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from blog.posts.models import Post
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    posts = models.ManyToManyField(Post)
+
+    def __unicode__(self):
+        return self.name
